e644c7bdc582e687e11d514ad2c462b15d360ffc
Ryan Reyes <ryan.david.reyes@gmail.com>
2018-05-23 16:26:12 -0400

    Http requests cool.

--- mainpage.elm
+++ mainpage.elm
@@ -1,11 +1,13 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode as Decode
 import Random
 
 main =
   Html.program
-    { init = init
+    { init = init "clouds"
     , view = view
     , update = update
     , subscriptions = subscriptions
@@ -13,31 +15,58 @@ main =
 
 -- Model
 type alias Model =
-  { dieFace : Int
+  { topic : String
+  , gifUrl : String
   }
+
+
+-- Init
+
+init : String -> (Model, Cmd Msg)
+init topic =
+  (Model topic "waiting.gif", getRandomGif topic)
 
 -- Update
 
 type Msg
-  = Roll
-  | NewFace Int
+  = Fetch
+  | NewGif (Result Http.Error String)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Roll ->
-      (model, Random.generate NewFace (Random.int 1 6))
+    Fetch ->
+      (model, getRandomGif model.topic)
 
-    NewFace newFace ->
-      (Model newFace, Cmd.none)
+    NewGif (Ok newUrl) ->
+      ( { model | gifUrl = newUrl }, Cmd.none )
+
+    NewGif (Err _) ->
+      (model, Cmd.none)
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let
+      url =
+        "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+
+      request =
+        Http.get url decodeGifUrl
+  in
+     Http.send NewGif request
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+  Decode.at ["data", "image_url"] Decode.string
 
 -- View
 
 view : Model -> Html Msg
 view model =
   div []
-  [ h1 [] [ text (toString model.dieFace ) ]
-  , button [onClick Roll ] [ text "Roll"]
+  [ h2 [] [ text (toString model.topic ) ]
+  , img [ src model.gifUrl ] []
+  , button [ onClick Fetch ] [ text "Fetch" ]
   ]
 
 -- Subscriptions
@@ -45,8 +74,3 @@ view model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
-
--- Init
-init : (Model, Cmd Msg)
-init =
-  (Model 1, Cmd.none)

2784042a7ba92235daa00f466d2435569d61729d
Ryan Reyes <ryan.david.reyes@gmail.com>
2018-05-23 15:46:02 -0400

    passwords

--- mainpage.elm
+++ mainpage.elm
@@ -1,6 +1,6 @@
-import Html exposing (Html, button, div, text, input)
-import Html.Events exposing (onClick, onInput)
+import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 
 
 main =
@@ -8,28 +8,52 @@ main =
 
 -- Model
 type alias Model =
-  { content : String
+  { name : String
+  , password : String
+  , passwordAgain : String
   }
 
 model : Model
 model =
-  { content = "" }
+  Model "" "" ""
 
 -- Update
 
-type Msg = Change String
+type Msg
+  = Name String
+  | Password String
+  | PasswordAgain String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Change newContent ->
-      { model | content = newContent }
+    Name name ->
+      { model | name = name }
+
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain passwordAgain ->
+      { model | passwordAgain = passwordAgain }
 
 -- View
 
 view : Model -> Html Msg
 view model =
   div []
-  [ input [ placeholder "Text to reverse", onInput Change ] []
-  , div [] [ text (String.reverse model.content) ]
+  [ input [ type_ "text", placeholder "Name", onInput Name ] []
+  , input [ type_ "password", placeholder "Password", onInput Password ] []
+  , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+  , viewValidation model
   ]
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  let
+      (color, message) =
+        if model.password == model.passwordAgain then
+           ("green", "OK")
+         else
+           ("red", "Passwords no match")
+  in
+     div [ style [("color", color)] ] [ text message ]

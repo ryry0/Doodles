081c828010cf9c525b2f73abd1f2f0591b67b5ea
Ryan Reyes <ryan.david.reyes@gmail.com>
2018-05-23 15:40:21 -0400

    Reversible form

--- mainpage.elm
+++ mainpage.elm
@@ -1,35 +1,35 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, text, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (..)
 
 
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 -- Model
-type alias Model = Int
+type alias Model =
+  { content : String
+  }
+
 model : Model
 model =
-  0
+  { content = "" }
 
 -- Update
 
-type Msg = Increment | Decrement
+type Msg = Change String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
+    Change newContent ->
+      { model | content = newContent }
 
 -- View
 
 view : Model -> Html Msg
 view model =
   div []
-  [ button [ onClick Decrement ] [ text "-" ]
-  , div [] [ text (toString model) ]
-  , button [ onClick Increment ] [ text "+" ]
+  [ input [ placeholder "Text to reverse", onInput Change ] []
+  , div [] [ text (String.reverse model.content) ]
   ]
